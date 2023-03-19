46d1c4e4d80a769d5c800a90c08931fd0ba56167
TheophilMirzoev <115739938+TheophilMirzoev@users.noreply.github.com>
2023-03-19 10:36:15 +0300

    f

---
+++ src/main/java/me/theo/recipeapp/controllers/FirstController.java
@@ -0,0 +1,10 @@
+package me.theo.recipeapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+}
+
---
+++ src/test/java/me/theo/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.theo.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
