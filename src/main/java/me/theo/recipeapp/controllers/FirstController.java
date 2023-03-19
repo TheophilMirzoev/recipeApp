810112d2c48435a0c013d4e99d76bf521281aecd
TheophilMirzoev <115739938+TheophilMirzoev@users.noreply.github.com>
2023-03-19 10:38:18 +0300

    f

--- src/main/java/me/theo/recipeapp/controllers/FirstController.java
+++ src/main/java/me/theo/recipeapp/controllers/FirstController.java
@@ -5,6 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
+    @GetMapping("/")
+    public String applicationIsRunning(){
+        return "Приложение запущено";
+    }
 
+    @GetMapping("/info")
+    public String info() {
+        return "name = Theo " + "//" + " projectName = recipe App " + "//" + "dateOfCreate = 19/03/23"+ "//" + "description = recipe app ";
+    }
 }
 
